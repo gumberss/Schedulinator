dd07bfcdf32c6736da7df3ed4cb6277bb9f5ecf4
gumberss <gustavomarinsuppi@gmail.com>
2023-09-23 12:32:13 -0300

    feat: adding schemas module

--- src/schemas.rs
+++ src/schemas.rs
@@ -1,1 +1,17 @@
 pub mod components;
+pub mod models {
+    pub mod task;
+}
+
+pub mod wire_in {
+    pub mod task;
+}
+
+pub mod wire_out {
+    pub mod db {
+        pub mod task;
+    }
+    pub mod redis {
+        pub mod task;
+    }
+}
