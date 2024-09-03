707c8147a77b8bc436e278e3985b2fe18d99f69b
gumberss <gustavomarinsuppi@gmail.com>
2024-09-02 22:32:01 -0300

    feat: refactor

--- src/controllers/execution.rs
+++ src/controllers/execution.rs
@@ -1,71 +1,32 @@
-use crate::adapters;
+use crate::diplomat::redis::task;
 use crate::logic;
 use crate::schemas::models::task_execution::TaskExecutionData;
-use actix_web::Error;
 use deadpool_redis::redis::cmd;
 use deadpool_redis::redis::RedisError;
 use deadpool_redis::redis::Value;
-use deadpool_redis::Connection;
 use deadpool_redis::Pool;
-use futures::future::join_all;
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
 use reqwest::Client;
-use std::collections;
-use tokio::task;
 
 pub async fn execute_tasks(redis_pool: &Pool) -> Result<Vec<TaskExecutionData>, String> {
-    let mut conn: Connection = redis_pool.get().await.unwrap();
-
-    let lua_script = r#"
-       
-    local currentServerTime = redis.call('TIME')
-    local thresholdScore = tonumber(currentServerTime[1]) 
-    local updatedKeysWithScores = redis.call('ZRANGEBYSCORE', 'schedules', 0, thresholdScore, 'WITHSCORES', 'LIMIT', 0, 5)
-    local newScores = {}
-    
-    for i=1, #updatedKeysWithScores, 2 do
-        local member = updatedKeysWithScores[i]
-        local score = tonumber(updatedKeysWithScores[i + 1])
-        local lock_score = score * -1
-        local hidrated = redis.call('GET', 'task_'..member)
-        redis.call('ZADD', 'schedules', lock_score, member)
-        table.insert(newScores, {member, score, hidrated})
-    end
-    
-    return newScores
-    
-    "#;
-
-    let keys = ["schedules"];
-
-    let response: Result<Value, RedisError> = cmd("EVAL")
-        .arg(lua_script)
-        .arg(1)
-        .arg(keys.len())
-        .arg(&keys)
-        .query_async(&mut conn)
-        .await;
+    let tasks_datas_response = task::get_tasks_to_be_processed(redis_pool).await;
 
     let client = reqwest::Client::new();
 
-    let updated_keys = match response {
-        Ok(Value::Bulk(items)) => {
-            dbg!(items.clone());
-            let mut tasks = adapters::wire_in::task_execution::bulk_cache_to_model(items)
+    let updated_keys = match tasks_datas_response {
+        Ok(tasks_datas) => {
+            dbg!(tasks_datas.clone());
+            let mut tasks = tasks_datas
                 .into_iter()
                 .map(|execution_data: TaskExecutionData| {
                     let client = client.clone();
-                    let local_pool = redis_pool.clone();
-
-                    async move { process(execution_data, client, &local_pool).await }
+                    async move { process(execution_data, client.clone(), &redis_pool.clone()).await }
                 })
                 .collect::<FuturesUnordered<_>>();
 
-            // Collect results from the spawned tasks
             let mut successes = Vec::new();
 
-            // Process each task as they complete
             while let Some(result) = tasks.next().await {
                 match result {
                     Ok(data) => successes.push(data),
@@ -75,8 +36,8 @@ pub async fn execute_tasks(redis_pool: &Pool) -> Result<Vec<TaskExecutionData>, 
 
             Ok(successes)
         }
-        Err(e) => Err(format!("Redis error: {:?}", e)),
-        _ => Err("Unexpected response format from Lua script".to_string()),
+        Err(e) => Err(e),
+        _ => Err("Unexpected error".to_string()),
     };
 
     dbg!(&updated_keys);
